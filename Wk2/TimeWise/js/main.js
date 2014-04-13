b5df0101dbae17ab29885105ba7fd589e72c2e92
Stacy Faude <istacy@fullsail.edu>
2014-04-13 09:53:38 -0500

    TimeWise - NEW -
    Started Over….

---
+++ Wk2/TimeWise/js/main.js
@@ -0,0 +1,74 @@
+/*  
+	TimeWise
+	Author: Stacy
+*/
+
+(function($){
+	
+	/*
+	===============================================
+	=========================== APPLICATION GLOBALS	
+	*/
+	
+	var win = $(window),
+		body = $(document.body),
+		container = $('#container'),	// the only element in index.html
+		currentUser = {}
+	;
+	
+	
+	/*
+	===============================================
+	========================= APPLICATION FUNCTIONS	
+	*/
+	
+	
+	var checkLoginState = function(){
+		$.ajax({
+			url: 'xhr/check_login.php',
+			type: 'get',
+			dataType: 'json',
+			success: function(response){
+				// if user, loadApp()
+				// if error, loadLanding()
+			}
+		});
+	};
+	
+	
+
+	// 	============================================
+	//	SETUP FOR INIT
+		
+	var init = function(){
+	
+		checkLoginState();
+	};
+	
+	
+	init();
+	
+		
+	/*
+	===============================================
+	======================================== EVENTS	
+	*/
+	
+	win.on('submit', '#user-reg-form', function(){
+		
+		return false;
+	});
+	
+	/*	
+	==================================== END EVENTS 
+	===============================================
+	*/
+		
+		
+
+	
+})(jQuery); // end private scope
+
+
+
+
